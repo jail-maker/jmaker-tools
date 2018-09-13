317484e1cceae5cc0ea02c0ea5930d3026dcda2b
Igor Bykov <igorb.dev@websm.io>
2018-09-13 17:18:23 +0300

    create rest server

--- index.js
+++ index.js
@@ -2,9 +2,71 @@
 
 'use strict';
 
-const yargs = require('yargs');
+const http = require('http');
+const Router = require('koa-better-router')();
+const Koa = require('koa');
 
-const argv = yargs
-    .commandDir('commands')
-    .demandCommand()
-    .argv
+const router = Router.loadMethods();
+const app = new Koa;
+
+router.post('/containers/builder', async ctx => {
+
+    let body = {
+        tty: false,
+        force: false,
+        contextPath: '',
+        manifestPath: '',
+    }
+
+    ctx.body = body;
+    ctx.type = 'application/json';
+
+});
+
+router.post('/containers/started', async ctx => {
+
+    let body = {
+        tty: false,
+        rm: false,
+        nat: false,
+        rules: {},
+        from: '',
+        name: '',
+        env: {},
+        mounts: [
+            {src: '', dest: ''},
+        ],
+        volumes: [
+            {name: '', dest: ''},
+        ],
+        entry: '',
+        command: '',
+    }
+
+    ctx.body = body;
+    ctx.type = 'application/json';
+
+});
+
+router.delete('/containers/started/:container', async ctx => {
+
+    ctx.body = body;
+    ctx.type = 'application/json';
+
+});
+
+router.delete('/containers/list/:container', async ctx => {
+
+    let body = {
+        force: false,
+    };
+
+    ctx.body = body;
+    ctx.type = 'application/json';
+
+});
+
+
+app.use(router.middleware());
+
+http.createServer(app.callback()).listen(3346, '127.0.0.1');
